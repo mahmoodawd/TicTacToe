1b1489fc4fe51a9266db9a6f5ef9d69151894e4b
AbdelrahmanEsam <abdelrahmanesam20000@gmail.com>
2023-05-08 22:08:47 +0300

    Revert "Merge pull request #19 from AbdelrahmanEsam/availablePlayersFeature_3WD"
    This reverts commit 5f86d1cedb0f7ca543ab71892fbdddea0e05d99e, reversing
    changes made to b2e82b60836f1951b52721a7cd3c158798e0a0b6.

--- src/tictactoe/available_players/data/AvailablePlayersRepoImp.java
+++ src/tictactoe/available_players/data/AvailablePlayersRepoImp.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tictactoe.available_players.data.repositories;
+package tictactoe.available_players.data;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,30 +25,25 @@ public class AvailablePlayersRepoImp implements AvailablePlayersRepoInterface {
     }
 
     @Override
-    public List<Player> getAvailablePlayers() {        
-        fetchPlayersListFromServer();
-        List<Player> players = new ArrayList();
-        players.addAll(availablePlayers);
+    public ObservableList<Player> getAvailablePlayers() {
+        fetchPlayersList();
+        ObservableList<Player> players = FXCollections.observableArrayList();
+        players.setAll(availablePlayers);
         return players;
     }
 
-    private void fetchPlayersListFromServer() {
+    public void fetchPlayersList() {
         //pretend to get players list from server
 
         for (int i = 0; i < 100; i++) {
             availablePlayers.add(new Player("Player " + i));
         }
     }
-    @Override
+    
     public boolean sendRequest(Player requester, Player receiver){
         //Send playing request to the receiver
         System.out.println(receiver.getName());
         return false ;
-    }
-    @Override
-    public boolean listenForRequest(){
-        boolean result = true; //should be retrieved from the server
-        return result;
     }
     }
 
