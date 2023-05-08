d1cc6b5f0eb95a8bdd0796ff3ecb2eda9546a933
Ma7moud3wd <mahmooodawd@gmail.com>
2023-05-08 19:05:48 +0300

    fix Build Issues

--- src/tictactoe/core/Remote.java
+++ src/tictactoe/core/Remote.java
@@ -35,7 +35,6 @@ public class Remote extends Thread{
             
              while(true)
              {
-                 
                String input = listener.readLine();
                switch(input.split(" ")[0])
                {
